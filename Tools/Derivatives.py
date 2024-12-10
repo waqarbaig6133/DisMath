d04dc856e6fa9c4a6e60ab1c26bc0914108ec513
Onii-Chan <140295418+waqarbaig6133@users.noreply.github.com>
2024-12-10 02:09:21 +0000

    Update Derivatives.py

--- Tools/Derivatives.py
+++ Tools/Derivatives.py
@@ -4,29 +4,64 @@ class derivative:
     def __init__(self, inputs):
         self.inputs = inputs
     def poly(self):
-        new_func = re.split(r'[+-]', self.inputs)
-        signs = [a for a in self.inputs if a == '-' or a == '+']
-        if '' in new_func: #Remove '' in list from seperation of string including - first coefficient
-            new_func.remove('')
-            
-        for x in new_func: #polynomials
-            if 'x' in x: #Works if it has a variable
-                if '^' in x: #If term has a degree >=2
-                    exponent = int(x[x.index('^')+1:])
-                    coefficient = int(x[:x.index('x')])
-                    new_func[new_func.index(x)] = x.replace(str(coefficient),'')
-                    x = x.replace(str(exponent),'')
-
-
-                else:
-                    x = x.replace('x','') #Remove the x in first degree monomial
-                   
+        def normal_der(self):
+            if 'x' not in self.inputs:
+                print(0)
             else:
-                new_func.remove(x) #Remove the constant from the split string
-                signs.pop() #This will remove the sign related to the constant
-                   
-derivative('2x^2+6x+9').poly()
+                new_func = [x for x in re.split(r'[()]', self.inputs) if x != '']        
+                for x in new_func:
+                    if 'x' not in x and x not in ['+', '-']:
+                        new_func.remove(x)
+                        new_func.pop()
+                    else:
+                        if x not in ['+','-']:
+                            if '^' in x:
+                                new_func[new_func.index(x)] = f"{str(int(x[:x.index('x')])*int(x[x.index('^')+1:]))}x^{int(x[x.index('^')+1:])-1}"
+                            else:
+                                new_func[new_func.index(x)] = f"{int(x[:x.index('x')])}"
+                for x in new_func[2:]:
+                    if x not in ['+','-']:
+                        if 'x' in x:
+                            if int(x[:x.index('x')]) < 0 and new_func[new_func.index(x)-1] == '+':
+                                new_func.remove(new_func[new_func.index(x)-1])
+                        else:
+                            if int(x) < 0 and new_func[new_func.index(x)-1] == '+':
+                                new_func.remove(new_func[new_func.index(x)-1])
+                print(''.join(new_func))
+derivative('[(2x^2)+(6x)]*[').poly()
+                        
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 
 import re
 
@@ -34,19 +69,41 @@ class derivative:
     def __init__(self, inputs):
         self.inputs = inputs
     def poly(self):
-        new_func = [x for x in re.split(r'[()]', self.inputs) if x != '']
-        signs = [a for a in self.inputs if a == '-' or a == '+']
+        if 'x' not in self.inputs:
+            print(0)
+        else:
+            new_func = [x for x in re.split(r'[()]', self.inputs) if x != '']        
+            for x in new_func:
+                if 'x' not in x and x not in ['+', '-']:
+                    new_func.remove(x)
+                    new_func.pop()
+                else:
+                    if x not in ['+','-']:
+                        coefficient = int(x[:x.index('x')])
+                        if '^' in x:
+                            exponent = int(x[x.index('^')+1:])
+                            new_func[new_func.index(x)] = f"{str(coefficient*exponent)}x^{exponent-1}"
+                        else:
+                            new_func[new_func.index(x)] = f"{coefficient}"
+            for x in new_func[2:]:
+                if x not in ['+','-']:
+                    if 'x' in x:
+                        a = int(x[:x.index('x')])
+                        b =new_func[new_func.index(x)-1]
+                        if a < 0 and b == '+':
+                            new_func.remove(new_func[new_func.index(x)-1])
+                    else:
+                        if int(x) < 0 and b == '+':
+                            new_func.remove(new_func[new_func.index(x)-1])
+                        
+            print(''.join(new_func))
+    def log(self):
         
-        
-
-        print(new_func)
-        print(signs)
-derivative('(2x^-2)+(6x^4)+(9)').poly()
+derivative('(5x)-(3)').poly()
 
 
 
-
-
+'''
 
 '''
 Sources:
@@ -65,4 +122,30 @@ https://docs.python.org/3/library/re.html
 https://www.geeksforgeeks.org/python-split-multiple-characters-from-string/
 
 
+
+def poly(self):
+        if 'x' not in self.inputs:
+            print(0)
+        else:
+            new_func = [x for x in re.split(r'[()]', self.inputs) if x != '']        
+            for x in new_func:
+                if 'x' not in x and x not in ['+', '-']:
+                    new_func.remove(x)
+                    new_func.pop()
+                else:
+                    if x not in ['+','-']:
+                        if '^' in x:
+                            new_func[new_func.index(x)] = f"{str(int(x[:x.index('x')])*int(x[x.index('^')+1:]))}x^{int(x[x.index('^')+1:])-1}"
+                        else:
+                            new_func[new_func.index(x)] = f"{int(x[:x.index('x')])}"
+            for x in new_func[2:]:
+                if x not in ['+','-']:
+                    if 'x' in x:
+                        if int(x[:x.index('x')]) < 0 and new_func[new_func.index(x)-1] == '+':
+                            new_func.remove(new_func[new_func.index(x)-1])
+                    else:
+                        if int(x) < 0 and new_func[new_func.index(x)-1] == '+':
+                            new_func.remove(new_func[new_func.index(x)-1])
+                        
+            print(''.join(new_func))
 '''
